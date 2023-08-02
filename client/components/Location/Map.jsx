c082d2a886c8b575cb158cba6126ecff310f2e9b
Jinyang-park <dbsskdud60@gmail.com>
2023-08-02 22:59:20 +0900

    [doc] 줌 컨트롤, 마커 생성 완료 및 마커 커스텀 중

--- client/components/Location/Map.jsx
+++ client/components/Location/Map.jsx
@@ -1,20 +1,44 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import Cake from '../../public/assets/cake.png';
 export default function Map() {
   useEffect(() => {
     const { naver } = window;
 
-    const mapOptions = {
-      center: new naver.maps.LatLng(36.63674569999992, 127.46559329999975),
-      zoom: 19,
+    // 지도의 위치, 위도 경도 파라미터로 넣어줌
+    const location = new naver.maps.LatLng(
+      36.63674569999992,
+      127.46559329999975
+    );
+
+    const map = new naver.maps.Map('map', {
+      center: location,
+      zoom: 16,
+      // 줌 컨트롤
+      zoomControl: true,
+      zoomControlOptions: {
+        style: naver.maps.ZoomControlStyle.SMALL,
+        position: naver.maps.Position.TOP_RIGHT,
+      },
+    });
+    // const content = [
+    //   '<div>',
+    //   `       <img src="../../public/assets/cake.png" width="85" height="85" alt="치즈본"/>`,
+    //   '</div>',
+    // ].join('');
+
+    const markerOptions = {
+      position: location,
+      map: map,
+      icon: {
+        content:
+          '<img src="https://img.icons8.com/?size=30&id=RdWZlLcQH7qw&format=png" alt="">',
+        size: new naver.maps.Size(22, 35),
+        origin: new naver.maps.Point(0, 0),
+        anchor: new naver.maps.Point(11, 35),
+      },
     };
 
-    const marker = new naver.maps.Marker({
-      position: new naver.maps.LatLng(36.6367457, 127.4655933),
-      map: map,
-    });
-
-    const map = new naver.maps.Map('map', mapOptions, marker);
+    const maker = new naver.maps.Marker(markerOptions);
   }, []);
 
   return (
