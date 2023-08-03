007ce7b6eddec1fce900186db3d4085cb71c5dab
Jinyang-park <dbsskdud60@gmail.com>
2023-08-03 20:52:48 +0900

    [doc] 마커 css 설정 및 링크 이동 작업 중

--- client/components/Location/Map.jsx
+++ client/components/Location/Map.jsx
@@ -1,6 +1,8 @@
-import React, { useEffect, useRef } from 'react';
-import Cake from '../../public/assets/cake.png';
-export default function Map() {
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+
+export default function Map({ post }) {
+  console.log(post);
   useEffect(() => {
     const { naver } = window;
 
@@ -10,9 +12,10 @@ export default function Map() {
       127.46559329999975
     );
 
+    const setloacation = new naver.maps.LatLng();
     const map = new naver.maps.Map('map', {
       center: location,
-      zoom: 16,
+      zoom: 18,
       // 줌 컨트롤
       zoomControl: true,
       zoomControlOptions: {
@@ -20,24 +23,36 @@ export default function Map() {
         position: naver.maps.Position.TOP_RIGHT,
       },
     });
-    // const content = [
-    //   '<div>',
-    //   `       <img src="../../public/assets/cake.png" width="85" height="85" alt="치즈본"/>`,
-    //   '</div>',
-    // ].join('');
 
+    var infowindow = new naver.maps.InfoWindow();
     const markerOptions = {
       position: location,
       map: map,
+
       icon: {
-        content:
-          '<img src="https://img.icons8.com/?size=30&id=RdWZlLcQH7qw&format=png" alt="">',
-        size: new naver.maps.Size(22, 35),
-        origin: new naver.maps.Point(0, 0),
-        anchor: new naver.maps.Point(11, 35),
+        content: [
+          '<div class="markerContainer" style="display: flex; padding:5px 19px 5px 30px; border-radius:23px; border: 2px solid #000000;     align-items: center; background: #fff;">',
+          '<div class="makercircle" style="display:table; margin-left: -20px; padding:7px; border-radius:15px;  background: #000000;">',
+          '<img src="https://img.icons8.com/?size=15&id=QqI85B8zokhd&format=png" style="filter: invert(100%) sepia(96%) saturate(1%) hue-rotate(9deg) brightness(104%) contrast(100%);"/>',
+          // postTitle,
+          '</div>',
+          '<h3 style="margin-left: 10px">치즈본</h3>',
+          '</div>',
+        ].join(''),
+        size: new naver.maps.Size(38, 58),
+        anchor: new naver.maps.Point(50, 58),
       },
     };
 
+    naver.maps.Event.addListener(markerOptions, 'click', function (e) {
+      url =
+        'https://map.naver.com/v5/search/%EC%B9%98%EC%A6%88%EB%B3%B8/place/1573099360?entry=plt&c=15.67,0,0,3,dh&isCorrectAnswer=true';
+
+      infowindow.open(url);
+    });
+    // naver.maps.Event.addListener(map, 'click', function (e) {
+    //   map.setPosition(e.coord);
+    // });
     const maker = new naver.maps.Marker(markerOptions);
   }, []);
 
@@ -49,3 +64,4 @@ export default function Map() {
     ></div>
   );
 }
+// https://map.naver.com/v5/search/%EC%B9%98%EC%A6%88%EB%B3%B8/place/1573099360?entry=plt&c=15.67,0,0,3,dh&isCorrectAnswer=true
