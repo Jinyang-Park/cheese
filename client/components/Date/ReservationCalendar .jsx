487b38dbe29d87ef27cf8b8a4efe8de097375e92
Jinyang-park <dbsskdud60@gmail.com>
2023-09-04 21:27:56 +0900

    [doc] 달력 커스텀 중

--- client/components/Date/ReservationCalendar .jsx
+++ client/components/Date/ReservationCalendar .jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
@@ -7,7 +8,14 @@ function ReservationCalendar() {
 
   return (
     <div>
-      <Calendar onChange={onChange} value={value} />
+      <Calendar
+        onChange={onChange}
+        formatDay={(locale, date) => moment(date).format('DD')} // 날'일' 제외하고 숫자만 보이도록 설정
+        showNeighboringMonth={false} //  이전, 이후 달의 날짜는 보이지 않도록 설정
+        next2Label={null} // >> 화살표 없앰
+        prev2Label={null} // << 화살표 없앰
+        value={value}
+      />
     </div>
   );
 }
